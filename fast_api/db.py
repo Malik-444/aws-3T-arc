39fa10e7712c63c282ee177b3d6fa01a83ff6a2e
Malik <151242422+Malik-444@users.noreply.github.com>
2026-01-27 22:39:07 -0500

    Change DATABASE_URL to use placeholders
    Updated DATABASE_URL format to use placeholders for credentials.

--- fast_api/db.py
+++ fast_api/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:password@db-endpoint:5432/dbname"
+DATABASE_URL = "postgresql://<USERNAME>:<PASSWORD>@<ENDPOINT>:5432/<DB_NAME>"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
